7ccbe4174d899b30f528bd4c2f714e186130637a
jaimesilvads <94398001+jaimesilvads@users.noreply.github.com>
2022-06-21 21:40:18 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Pipeline Jaime Silva executado com sucesso.
+        </p>
+        <a
+          className="App-link"
+          href="http://comunidadecloud.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Cloud Treinamentos - jaime Silva
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
